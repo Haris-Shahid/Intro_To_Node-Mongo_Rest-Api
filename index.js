a49b0f4b91ebeec2731ccaaabb8c5592c2f19ea9
Haris Shahid <harisshahid01@yahoo.com>
2018-03-26 23:04:01 +0500

    start running port 3000

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var app = express();
 app.get('/', function(req, res) {
     res.send('My first api')
 })
+
+app.listen(3000, function() {
+    console.log('first api start running on port 3000!')
+})
