824a9d8e5d71c6539c688cac5efbd9edf3aaf49c
Haris Shahid <harisshahid01@yahoo.com>
2018-03-26 23:07:09 +0500

    my first api part completed

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res) {
     res.send('My first api')
 })
 
+app.get('/api', function(req, res) {
+    res.send('yahoo! api is working')
+})
+
 app.listen(3000, function() {
     console.log('first api start running on port 3000!')
 })
