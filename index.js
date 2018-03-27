a613a8913d304269f7182dbc94db05699f226d9f
Haris Shahid <harisshahid01@yahoo.com>
2018-03-27 20:41:49 +0500

    use bodyparser which is node js middleware

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var ingredients = [
+    {
+        "id": 'dskljal',
+        "text": 'Eggs'
+    },
+    {
+        "id": 'dsklsfl',
+        "text": 'Milk'
+    },
+    {
+        "id": 'dsjksfl',
+        "text": 'cow'
+    },
+    {
+        "id": 'abjksfl',
+        "text": 'hen legs'
+    }
+]
 
 app.get('/', function(req, res) {
-    res.send('My first api')
+    res.send(ingredients)
 })
 
 app.get('/api', function(req, res) {
