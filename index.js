6aaccf04f73097af770484fa86274e29442d1b0f
Haris Shahid <harisshahid01@yahoo.com>
2018-03-29 22:32:44 +0500

    put request example completed

--- index.js
+++ index.js
@@ -24,24 +24,41 @@ var ingredients = [
     }
 ]
 
-app.get('/', function(req, res) {
+app.get('/ingredients', function (req, res) {
     res.send(ingredients)
 })
 
-app.post('/', function(req, res) {
-    var ingredient = req.body ;
-    if(!ingredient || ingredient.text === ""){
-        res.status(500).send({error: "Please type something"})
-    }else{
+app.post('/ingredients', function (req, res) {
+    var ingredient = req.body;
+    if (!ingredient || ingredient.text === "") {
+        res.status(500).send({ error: "Please type something" })
+    } else {
         ingredients.push(ingredient);
         res.status(200).send(ingredients)
     }
 })
 
-app.get('/api', function(req, res) {
+app.put('/ingredients/:ingredientId', function (req, res) {
+    var newText = req.body.text;
+
+    if (!newText || newText === "") {
+        res.status(500).send({ error: "Please write some ingredient" })
+    } else {
+        for (var i = 0; i < ingredients.length; i++) {
+            var ing = ingredients[i];
+            if (ing.id === req.params.ingredientId) {
+                ing.text = newText;
+                break;
+            }
+        }
+        res.send(ingredients);
+    }
+})
+
+app.get('/api', function (req, res) {
     res.send('yahoo! api is working')
 })
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('first api start running on port 3000!')
 })
