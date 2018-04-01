05bb2e9196810584a132c3302720274884cea1ee
Haris Shahid <harisshahid01@yahoo.com>
2018-04-01 12:16:00 +0500

    mongodb boilerplate

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+var db = mongoose.connect('mongodb://localhost/swag-shop')
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
