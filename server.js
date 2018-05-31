9c79e405335c27a1cada52fb0432f3f8d63be577
Haris Shahid <harisshahid01@yahoo.com>
2018-05-31 14:19:42 +0500

    Swg Shop API: Postiong Products

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ var db = mongoose.connect('mongodb://localhost/swag-shop')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.post('/product', function(req, res){
+    var product = new Product();
+    product.title = req.body.title;
+    product.price = req.body.price;
+    // product.likes = 0;
+    product.save(function(err, savedProduct){
+        if (err){
+            res.status(500).send({error: "Could not save product"});
+        }else{
+            res.send(savedProduct);
+        }
+    })
+})
+
 app.listen(3000, function () {
     console.log('first api start running on port 3000!')
 })
