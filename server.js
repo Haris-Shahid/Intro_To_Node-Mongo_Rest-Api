4188ff2349a689b25c3031c3f98445bf800009c9
Haris Shahid <harisshahid01@yahoo.com>
2018-05-31 14:02:37 +0500

    Swap Shop API: Creating the Model

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+var Product = require('./model/product');
+var WishList = require('./model/wishlist');
+
 var db = mongoose.connect('mongodb://localhost/swag-shop')
 
 app.use(bodyParser.json());
